06ccd9cc4622887c86efd17850afb3b33982e7dd
luizfnds <luizeduardofnds@gmail.com>
2023-06-23 16:40:18 -0300

    Update dbConfig.php

--- db/dbConfig.php
+++ db/dbConfig.php
@@ -1,4 +1,4 @@
 <?php
-    $pdo = new PDO("mysql://root:E37ESXcDpgE5EOEOAqKm@containers-us-west-148.railway.app:5554/railway", "root" ,"E37ESXcDpgE5EOEOAqKm");
+    $pdo = new PDO("mysql:host=containers-us-west-148.railway.app;dbname=railway", "root" ,"E37ESXcDpgE5EOEOAqKm");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
